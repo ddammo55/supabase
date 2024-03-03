9a033775d2b59deece788dbdc80a9a6ced54604e
Won ho Choi <wonhochoi@Wonui-MacBookPro.local>
2024-03-03 11:11:35 +0900

    stripe

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -2,6 +2,11 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { CheckCircle2 } from 'lucide-react';
 import React from 'react';
+import prisma from '@/app/lib/db';
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import { getStripeSession } from '@/app/lib/stripe';
+import { redirect } from 'next/navigation';
+import { StripeSubscriptionCreationButton } from '@/app/components/SubmitButton';
 
 const  featureItems = [
     {name: "Lorem Ipsum something"},
@@ -11,7 +16,60 @@ const  featureItems = [
     {name: "Lorem Ipsum something"},
 ]
 
-export default function BillingPage() {
+async function getData(userId: string){
+    const data = await prisma.subscription.findUnique({
+        where:{
+           userId:userId, 
+        },
+        select:{
+            status : true,
+            user : {
+                select:{
+                    stripeCustomerId : true,
+                }
+            }
+        }
+    })
+
+    return data;
+}
+
+export default async function BillingPage() {
+    /**
+     * Retrieves the user from the Kinde server session.
+     * Kinde 서버 세션에서 사용자를 검색합니다.
+     * @returns The user object.
+     */
+    const {getUser} = getKindeServerSession(); // Kinde 서버 세션에서 사용자를 검색합니다.
+    const user = await getUser(); // 사용자 정보를 가져옵니다.
+    const data = await getData(user?.id as string); // 사용자 정보를 가져옵니다.
+
+    async function  createSubscription(){// 구독 생성
+        "use server";// 서버 사용
+
+        const dbUser = await prisma.user.findUnique({
+            where : {
+                id : user?.id
+            },
+            select : {
+                stripeCustomerId : true
+            }
+        })
+
+
+        if(!dbUser?.stripeCustomerId){ // 고객 ID를 가져올 수 없습니다.
+            throw new Error("Unable to get customer id");// 고객 ID를 가져올 수 없습니다.
+        }
+
+        const subscriptionUrl = await getStripeSession({
+            customerId: dbUser.stripeCustomerId,
+            domainUrl  :   'http://localhost:3000',
+            priceId: process.env.STRIPE_PRICE_ID as string,
+        });// 스트라이프 세션을 가져옵니다.
+
+        return redirect(subscriptionUrl);// 구독 URL로 리디렉션합니다.
+    }
+
     return (
         <div className='max-w-md mx-auto space-x-4'>
     
@@ -39,8 +97,8 @@ export default function BillingPage() {
                         ))}
                     </ul>
 
-                    <form className='w-full'>
-                        <Button className='w-full'>Buy today</Button>
+                    <form className='w-full' action={createSubscription}>
+                        <StripeSubscriptionCreationButton/>
                     </form>
                 </div>
 
---
+++ app/payment/cancelled/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CancelledRoute() {
+    return (
+        <div className='w-full min-h-[80vh] flex items-center justify-center bg-slate-400'>
+            <h1>hhhh</h1>
+        </div>
+    );
+}
+
