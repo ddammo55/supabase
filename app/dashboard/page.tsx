28be9af4f2ffe22d02acd4ce460df6934d62a49e
Won ho Choi <wonhochoi@Wonui-MacBookPro.local>
2024-03-06 06:38:51 +0900

    dashbboard_page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -24,7 +24,7 @@ export default async function Dashboard() {
     const user = await getUser(); // 사용자 정보를 가져옵니다.
     const data = await getData(user?.id as string); // 사용자 정보를 가져옵니다.
     return (
-        <div className='grid items-start gapy-8'>
+        <div className='grid items-start gap-y-8'>
             <div className="flex items-center justify-between px-2">
                 <div className="grid gap-1">
                     <h1 className='text-3xl md:text-4xl'>Your Notes</h1>
@@ -38,9 +38,11 @@ export default async function Dashboard() {
 
             {data.length < 1 ? (
                 <div className='flex min-h-[400px] flex-col items-center justify-center border border-dashed p-8 text-center animate-in fade-in-50'>
-                    <div>
+                    <div className='flex h-20 w-20 items-center justify-center rounded-full bg-primary/10'>
                         <File className='w-10 h-10 text-primary'/>
                     </div>
+
+                    <h2 className='mt-6 text-xl font-semibold'>You dont have any notes created</h2>
                 </div>
             ):(
                 <div></div>
